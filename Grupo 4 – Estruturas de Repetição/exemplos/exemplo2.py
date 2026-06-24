3743cc4ea5c8a21e3c9c2832c08013dde22768a1
Emanuel Henrique <pemanuelhenrique@gmail.com>
2026-06-24 20:46:26 -0300

    docs: exemplos foram adicionados

---
+++ Grupo 4 – Estruturas de Repetição/exemplos/exemplo2.py
@@ -0,0 +1,9 @@
+contador = 0
+
+while contador <= 10:
+    contador += 1
+
+    if contador % 2 == 0:
+        continue
+    else:
+        print(contador)
---
+++ Grupo 4 – Estruturas de Repetição/exemplos/exemplo6.py
@@ -0,0 +1,2 @@
+for numero in range(1, 6, 1):
+    print(f"Repetida {numero} vezes")
